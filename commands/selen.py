6021de04ce8de856562f1f980b85e60f11532fee
rnshimiy <rnshimiy@umich.edu>
2022-12-21 00:53:43 -0500

    Wed Dec 21 00:53:43 EST 2022

--- commands/selen.py
+++ commands/selen.py
@@ -14,15 +14,21 @@ driver.get(shimmy)
 # Find all of the links on the page
 links = driver.find_elements(By.TAG_NAME, 'a')
 # Iterate through the links and click on each one
-for link in links:
+for link in links: 
     try:
         link.click()
         # Wait for the page to load
         driver.implicitly_wait(10)
         # Print the title of the page
-        print(driver.title, driver.current_url)
+        print(driver.title, driver.current_url, driver.execute_script(
+            "return document.readyState"))
+        if driver.current_url.startswith(shimmy):
+            # The link does not return a 404 error
+            print("The link does not return a 404 error.")
     except:
         print(driver.current_url ," == Error: Link did not lead to a valid page")
+        
+        
     # Navigate back to the blog page
     driver.get(shimmy)
 
